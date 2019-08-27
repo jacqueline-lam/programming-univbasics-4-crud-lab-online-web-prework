6858cda28a47212e2f2c1ebbf7945f1150dc22f8
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-08-27 22:09:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,8 @@ def create_an_empty_array
 end
 
 def create_an_array
-  Array.new = [a, b, c, d]
+ my_array =  Array.new
+ my_array = [a, b, c, d] 
 end
 
 def add_element_to_end_of_array(array, element)
